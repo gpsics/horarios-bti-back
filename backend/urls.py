381a6d3fd3c64bf20322361430825febb02f53e1
Leonardo_Dantas <l.dantas1155@gmail.com>
2023-08-06 09:04:17 -0300

    Preparando o ambiente para a integração inicial com o front-end

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from horarios.views import ComponenteCurricularViewSet, ProfessorViewSet, TurmaViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,6 +14,7 @@ router.register(r'turma', TurmaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
     path('admin/', admin.site.urls),
 ]
 
