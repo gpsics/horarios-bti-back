3e2bea58f5c78182b06f024c6ee55ab95e7cff9f
Leonardo_Dantas <l.dantas1155@gmail.com>
2023-12-17 18:42:03 -0300

    Modificando a lógica de formatação dos horários das turmas

--- horarios/api/serializers.py
+++ horarios/api/serializers.py
@@ -54,50 +54,70 @@ class TurmaSerializerFormatado(serializers.ModelSerializer):
         fields = ['id', 'cod_componente', 'num_turma', 'horario', 'num_vagas', 'professor']
 
     def get_horario(self, Turma):
-        vetor = []
-        vetor_horarios = Turma.horario.split()
-        model = r'^(\d+)([MTN])(\d+)$'
+        def estruturar_horario(horarios, recursive=True):
+            vetor_horarios = []
+            tokenize_horarios = horarios.split()
+            model = r'^(\d+)([MTN])(\d+)$'
 
-        for horario in vetor_horarios:
-            if vetor:
-                for index_temp, horario_temp in enumerate(vetor):
-                    if horario != horario_temp:
-                        # Separação dos dados do horário formatado
-                        match1 = re.match(model, horario_temp)
-                        dia1 = "".join(match1[1])
-                        turno1 = "".join(match1[2])
-                        hora1 = "".join(match1[3])
+            # Percorre o vetor para realizar a comparação
+            for horario in tokenize_horarios:
+                changed = False
 
-                        # Separação dos dados do horário a ser comparado
-                        match2 = re.match(model, horario)
-                        dia2 = "".join(match2[1])
-                        turno2 = "".join(match2[2])
-                        hora2 = "".join(match2[3])
+                # Verifica se vetor está vazio
+                if vetor_horarios:
+                    # Percorre o vetor para realizar a comparação
+                    for index_temp, horario_temp in enumerate(vetor_horarios):
+                        if horario != horario_temp and not changed:
+                            # Separação dos termos do horário formatado
+                            match1 = re.match(model, horario_temp)
+                            dia1 = "".join(match1[1])
+                            turno1 = "".join(match1[2])
+                            hora1 = "".join(match1[3])
 
-                        if turno1 == turno2:
-                            aux_dias = "".join(match1[1])
-                            aux_horas = "".join(match1[3])
-                            changed = False
+                            # Separação dos termos do horário a ser comparado
+                            match2 = re.match(model, horario)
+                            dia2 = "".join(match2[1])
+                            turno2 = "".join(match2[2])
+                            hora2 = "".join(match2[3])
 
-                            if hora1 == hora2:
-                                aux_dias = "".join(sorted(match1[1] + match2[1]))
-                                changed = True
+                            # Primeiramente compara se os horários estão no mesmo turno
+                            if turno1 == turno2:
+                                # Compara se as horas são iguais
+                                if hora1 == hora2:
+                                    # Junta os termos que representa os dias dos horários
+                                    dia1 = "".join(sorted(match1[1] + match2[1]))
+                                    changed = True
 
-                            if dia1 == dia2 and int(hora1[-1::]) + 1 == int(hora2[-1::]):
-                                aux_horas = "".join(sorted(match1[3] + match2[3]))
-                                changed = True
+                                # Compara se os dias são iguais
+                                if dia1 == dia2:
+                                    # Junta os termos que representa as horas dos horários
+                                    hora1 = "".join(sorted(match1[3] + match2[3]))
+                                    changed = True
 
-                            if not changed and (index_temp + 1) == len(vetor):
-                                vetor.append(horario)
-                            else:
-                                vetor[index_temp] = aux_dias + turno1 + aux_horas
+                                # Verifica se houve junção dos termos dos horários comparados
+                                if not changed and (index_temp + 1) == len(vetor_horarios):
+                                    # Se não houver mudança, o horário já é adicionado no vetor
+                                    vetor_horarios.append(horario)
+                                else:
+                                    # Se houver mudança, o horário é modificado no vetor
+                                    vetor_horarios[index_temp] = dia1 + turno1 + hora1
 
-                        elif (index_temp + 1) == len(vetor):
-                            vetor.append(horario)
-            else:
-                vetor.append(horario)
+                            elif (index_temp + 1) == len(vetor_horarios):
+                                # Se os horários tiveram turnos diferentes, o horário é adicionado no vetor
+                                vetor_horarios.append(horario)
+                else:
+                    # Se o vetor estiver vazio, o horário já é adicionado no vetor
+                    vetor_horarios.append(horario)
 
-        return " ".join(vetor)
+            # Se a opção 'recursive' for True, a função é chamada novamente
+            if recursive:
+                vetor_horarios = estruturar_horario(" ".join(vetor_horarios), False)
+
+            # Retorna o vetor dos horários
+            return vetor_horarios
+
+        # Retorna o horário formatado em uma String
+        return " ".join(estruturar_horario(Turma.horario))
 
 
 # Serializer dos dados de uma Turma (Simplificação da informação para exibir os horários)
