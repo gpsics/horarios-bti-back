d98f592e620608b895c3a1794159426fd71f88f3
Leonardo_Dantas <l.dantas1155@gmail.com>
2024-01-05 12:04:59 -0300

    Criando todas as validações dentro das views de todas as classes.

--- horarios/api/views.py
+++ horarios/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets, generics
-from rest_framework.response import Response
-from .serializers import ComponenteCurricularSerializer, ProfessorSerializer, TurmaSerializer, \
-    TurmaSerializerFormatado, HorariosSerializer, ConflitosSerializer
-from horarios.models import ComponenteCurricular, Professor, Turma
-
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 from decimal import Decimal
 import re
+
+from horarios.models import ComponenteCurricular, Professor, Turma
+from .serializers import ComponenteCurricularSerializer, ProfessorSerializer, TurmaSerializer, \
+    TurmaSerializerFormatado, HorariosSerializer, ConflitosSerializer
 
 
 # View que está mostrando todos os objetos criados de Componente Curricular
@@ -15,41 +16,83 @@ class ComponenteCurricularViewSet(viewsets.ModelViewSet):
     serializer_class = ComponenteCurricularSerializer
     permission_classes = [IsAuthenticated]
 
+    def validate_codigo_componente(self, codigo):
+        if not re.match(r'^([A-Z]{3})([0-9]{4})$', codigo):
+            return f"Formato inválido do código ({codigo})."
+
+    def validate_nome_componente(self, nome):
+        if nome == "" or len(nome) > 80:
+            return f"O nome do componente ({nome}) deve ter no mínimo 1 caractere e no máximo 80."
+
+    def validate_semestre_componente(self, semestre, obrigatorio):
+        if int(semestre) < 0:
+            return f"O número do semestre ({semestre}) deve ser maior que 0."
+
+        if int(semestre) != 0 and not obrigatorio:
+            return f"O componente deve ser obrigatório quando número do semestre diferente de 0."
+
+    def validate_carga_componente(self, carga):
+        if int(carga) < 0 or not int(carga) % 15 == 0:
+            return f'Carga horária ({carga}) deve maior que 0 e divisível por 15.'
+
+    def validate_componente(self, componente):
+        validation_errors = {}
+        codigo = self.validate_codigo_componente(componente.get("codigo").upper())
+        nome_comp = self.validate_nome_componente(componente.get("nome_comp").upper())
+        semestre = self.validate_semestre_componente(componente.get("num_semestre"), componente.get("obrigatorio"))
+        carga = self.validate_carga_componente(componente.get("carga_horaria"))
+
+        if codigo:
+            validation_errors['codigo'] = codigo
+
+        if nome_comp:
+            validation_errors['nome_comp'] = nome_comp
+
+        if semestre:
+            validation_errors['num_semestre'] = semestre
+
+        if carga:
+            validation_errors['carga_horaria'] = carga
+
+        return validation_errors
+
+    def perform_create_or_update(self, serializer):
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=201)
+        else:
+            return Response(data=serializer.errors, status=400)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            componente = self.queryset.get(pk=kwargs.get('pk'))
+        except ObjectDoesNotExist:
+            errors = {f'Componente Curricular não encontrado!'}
+            return Response(data=errors, status=400)
+
+        serializer = self.get_serializer(componente)
+        return Response(serializer.data)
+
     def create(self, request, *args, **kwargs):
         data = request.data
 
-        codigo = data.get("codigo")
-        nome = data.get("nome_comp")
-        semestre = data.get("num_semestre")
-        carga = data.get("carga_horaria")
-        obrigatorio = data.get("obrigatorio")
-
-        if not re.match(r'^([A-Z]{3})([0-9]{4})$', codigo):
-            errors = [f"Formato inválido do código ({codigo})."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
-
-        if nome == "" or len(nome) > 80:
-            errors = [f"O nome do componente ({nome}) deve ter no mínimo 1 caractere e no máximo 80."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
-
-        if semestre < 0:
-            errors = [f"O número do semestre ({semestre}) deve ser maior que 0."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
-
-        if semestre != 0 and not obrigatorio:
-            errors = [f"O componente deve ser obrigatório quando número do semestre diferente de 0."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
-
-        if carga < 0 or not carga % 15 == 0:
-            errors = [f'Carga horária ({carga}) deve maior que 0 e divisível por 15.']
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+        validation_errors = self.validate_componente(data)
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
 
         serializer = ComponenteCurricularSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=201)  # Retorno com status 201 Created
-        else:
-            return Response(data=serializer.errors, status=400)  # Retorno com erros de validação
+        return self.perform_create_or_update(serializer)
+
+    def update(self, request, *args, **kwargs):
+        componente = self.get_object()
+        data = request.data
+
+        validation_errors = self.validate_componente(data)
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
+
+        serializer = ProfessorSerializer(componente, data=data)
+        return self.perform_create_or_update(serializer)
 
 
 # View que está mostrando todos os objetos criados de Professor
@@ -58,25 +101,51 @@ class ProfessorViewSet(viewsets.ModelViewSet):
     serializer_class = ProfessorSerializer
     permission_classes = [IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        nome = data.get("nome_prof")
-
+    def validate_professor(self, nome):
         if len(nome) > 80:
-            errors = ["O nome do professor deve ter no máximo 80 caracteres."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f'O nome do professor deve ter no máximo 80 caracteres.'
 
         professores = Professor.objects.filter(nome_prof=nome)
         if professores.exists():
-            errors = ["O nome do professor deve ser único."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f'Já existe um professor com o nome de {nome}.'
 
-        serializer = ProfessorSerializer(data=data)
+    def perform_create_or_update(self, serializer):
         if serializer.is_valid():
             serializer.save()
-            return Response(data=serializer.data, status=201)  # Retorno com status 201 Created
+            return Response(data=serializer.data, status=201)
         else:
-            return Response(data=serializer.errors, status=400)  # Retorno com erros de validação
+            return Response(data=serializer.errors, status=400)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            professor = self.queryset.get(pk=kwargs.get('pk'))
+        except ObjectDoesNotExist:
+            errors = {f'Professor não encontrado!'}
+            return Response(data=errors, status=400)
+
+        serializer = self.get_serializer(professor)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        validation_errors = self.validate_professor(data.get("nome_prof").upper())
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
+
+        serializer = ProfessorSerializer(data=data)
+        return self.perform_create_or_update(serializer)
+
+    def update(self, request, *args, **kwargs):
+        professor = self.get_object()
+        data = request.data
+
+        validation_errors = self.validate_professor(data.get("nome_prof").upper())
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
+
+        serializer = ProfessorSerializer(professor, data=data)
+        return self.perform_create_or_update(serializer)
 
 
 # View que está mostrando todos os objetos criados de Turma
@@ -90,51 +159,139 @@ class TurmaViewSet(viewsets.ModelViewSet):
 
         return TurmaSerializer
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-
-        codigo = data.get("cod_componente")
-        num_turma = data.get("num_turma")
-        horario = data.get("horario")
-        num_vagas = data.get("num_vagas")
-        professores = data.get("professor")
-
+    def validate_codigo_turma(self, codigo):
         if not re.match(r'^([A-Z]{3})([0-9]{4})$', codigo):
-            errors = [f"Formato inválido do código ({codigo})."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f"Formato inválido do código ({codigo})."
 
         componente = ComponenteCurricular.objects.filter(codigo=codigo).first()
         if not componente:
-            errors = [f"Não existe componente curricular com esse código ({codigo})."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f"Não existe componente curricular com esse código ({codigo})."
 
+    def validate_numero_turma(self, codigo, num_turma):
         if num_turma < 0:
-            errors = [f"O número da turma ({num_turma}) deve ser positivo."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f"O número da turma ({num_turma}) deve ser positivo."
 
         turma = Turma.objects.filter(cod_componente=codigo, num_turma=num_turma)
         if turma.exists():
-            errors = [f"Já existe turma com esse código e número ({codigo} - {num_turma})."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+            return f"Já existe uma turma com esse código e número ({codigo} - {num_turma})."
+
+    def validate_horario_turma(self, horarios, carga_horaria):
+        vetor_horarios = list(set(horarios.split()))
+        contador_h = 0
+
+        # Verifica se o horário está dentro do limite para a carga horária do componente
+        if not len(vetor_horarios) == carga_horaria / 15:
+            return f'Horário ({horarios}) inválido para a turma.'
+
+        for index in range(len(vetor_horarios)):
+            # Verifica se o horário está seguindo a sua expressão regular ou seu modelo
+            if not re.match(r'^[2-7]+[MmTtNn][1-6]+$', vetor_horarios[index]):
+                return f'Formato inválido do horário ({vetor_horarios[index]}).'
+
+            # Ordena de forma crescente as partes contendo número na expressão
+            dias_ordenado = "".join(sorted(re.sub(r'[MmNnTt].*', '', vetor_horarios[index])))
+            horas_ordenado = "".join(sorted(re.sub(r'^.*[MmNnTt]', '', vetor_horarios[index])))
+
+            # Verifica se nas partes contendo números, contém apenas números
+            if re.search(r'[a-zA-Z]', dias_ordenado) or re.search(r'[a-zA-Z]', horas_ordenado):
+                return f'Formato inválido do horário ({vetor_horarios[index]}).'
+
+            # Incrementa a quantidade de horas no horario no contador_h
+            contador_h += len(dias_ordenado) * len(horas_ordenado)
+
+            turno = (re.search(r'[MmNnTt]', vetor_horarios[index])).group().upper()
+
+            # Após as informações serem ordenadas e verificadas, são postas na string novamente
+            vetor_horarios[index] = dias_ordenado + turno + horas_ordenado
+
+        # Verifica se a quantidade de horas presente no horário está correto
+        if not contador_h == carga_horaria / 15:
+            return f'A quantidade de horas no horário está inválida ({contador_h} - {carga_horaria}).'
+
+    def validate_vagas_turma(self, num_vagas):
+        if num_vagas < 0:
+            return f"O número de vagas ({num_vagas}) deve ser positivo."
+
+    def validate_professores_turma(self, codigo, professores):
+        componente = ComponenteCurricular.objects.filter(codigo=codigo).first()
 
         for id_professor in professores:
             professor = Professor.objects.get(pk=id_professor)
 
             if componente:
                 if (professor.horas_semanais + Decimal(componente.carga_horaria / 15)) > 20:
-                    errors = [f"Quantidade de horas semanais máxima do professor ({professor}) atinginda."]
-                    return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+                    return f"Quantidade máxima de horas semanais do professor ({professor}) alcançada."
 
-        if num_vagas < 0:
-            errors = [f"O número de vagas ({num_vagas}) deve ser positivo."]
-            return Response(data=errors, status=400)  # Retorno com status 400 Bad Request
+    def validate_turma(self, turma):
+        validation_errors = {}
 
-        serializer = TurmaSerializer(data=data)
+        cod_componente = self.validate_codigo_turma(turma.get("cod_componente").upper())
+        num_turma = self.validate_numero_turma(turma.get("cod_componente"), turma.get("num_turma"))
+        vagas = self.validate_vagas_turma(turma.get("num_vagas"))
+
+        componente = ComponenteCurricular.objects.filter(codigo=turma.get("cod_componente").upper()).first()
+        if componente:
+            horario = self.validate_horario_turma(turma.get("horario").upper(), componente.carga_horaria)
+            if horario:
+                validation_errors['horario'] = horario
+
+        try:
+            check_prof = turma.get("professor")
+            if check_prof:
+                professor = self.validate_professores_turma(check_prof)
+                if professor:
+                    validation_errors['professor'] = professor
+        except ObjectDoesNotExist:
+            pass
+
+        if cod_componente:
+            validation_errors['cod_componente'] = cod_componente
+
+        if num_turma:
+            validation_errors['num_turma'] = num_turma
+
+        if vagas:
+            validation_errors['vagas'] = vagas
+
+        return validation_errors
+
+    def perform_create_or_update(self, serializer):
         if serializer.is_valid():
             serializer.save()
-            return Response(data=serializer.data, status=201)  # Retorno com status 201 Created
+            return Response(data=serializer.data, status=201)
         else:
-            return Response(data=serializer.errors, status=400)  # Retorno com erros de validação
+            return Response(data=serializer.errors, status=400)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            turma = self.queryset.get(pk=kwargs.get('pk'))
+        except ObjectDoesNotExist:
+            errors = {"Turma não encontrado!"}
+            return Response(data=errors, status=400)
+
+        serializer = self.get_serializer(turma)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        validation_errors = self.validate_turma(data)
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
+
+        serializer = TurmaSerializer(data=data)
+        return self.perform_create_or_update(serializer)
+
+    def update(self, request, *args, **kwargs):
+        turma = self.get_object()
+        data = request.data
+
+        validation_errors = self.validate_turma(data)
+        if validation_errors:
+            return Response(data=validation_errors, status=400)
+
+        serializer = TurmaSerializer(turma, data=data)
+        return self.perform_create_or_update(serializer)
 
 
 # APIView que mostra todos os horários de Turmas com mesmo componentes
